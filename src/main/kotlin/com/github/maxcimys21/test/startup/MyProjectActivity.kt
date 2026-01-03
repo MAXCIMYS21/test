a670f49db9b1a662d3832a7484ede735d5926203
GitHub Action <action@github.com>
2026-01-03 15:56:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/maxcimys21/test/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/maxcimys21/test/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.maxcimys21.test.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
